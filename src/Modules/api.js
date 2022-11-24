c6850fa34afddbb70e44c5dd03634adc4044a8f5
Nabeel Akbar <studiosnabeel@gmail.com>
2022-11-24 19:04:06 +0500

    typo error remove

--- src/Modules/api.js
+++ src/Modules/api.js
@@ -7,34 +7,34 @@ const baseUrl =
 const apiUrl = `${baseUrl}${apiKey}/scores`;
 
 const addData = async (player, score) => {
-  await fetch (apiUrl, {
+  await fetch(apiUrl, {
     method: 'POST',
     body: JSON.stringify({
-     user: player,
-     score: score,
+      user: player,
+      score: score,
     }),
     headers: {
-      'content-type': 'application/json'; charset='UTF-8',
+      'content-type': 'application/json; charset=UTF-8',
     },
   })
-  .then((response)=> response.json())
-  .then((json)=> {
-    const alrt = document.querySelector('h4');
-    alrt.innerHTML = json.result;
-    alrt.style.backgroundColor = '#fff';
-    alrt.style.color = 'red';
-    setTimeout(()=> {
-      alrt.style.display = 'none';
-}, 4000);
-  });
+    .then((response) => response.json())
+    .then((json) => {
+      const alrt = document.querySelector('h4');
+      alrt.innerHTML = json.result;
+      alrt.style.backgroundColor = '#fff';
+      alrt.style.color = 'red';
+      setTimeout(() => {
+        alrt.style.display = 'none';
+      }, 4000);
+    });
 };
 
-const AddScoreList = async ()=> {
-  await fetch (apiUrl)
-  .then((response)=> response.json())
-  .then((json)=> {
-    addTableData(json.result.sort((x, y)=> y.score - x.score));
-  });
+const AddScoreList = async () => {
+  await fetch(apiUrl)
+    .then((response) => response.json())
+    .then((json) => {
+      addTableData(json.result.sort((x, y) => y.score - x.score));
+    });
 };
 
-export { addData,AddScoreList};
+export { addData, AddScoreList };
