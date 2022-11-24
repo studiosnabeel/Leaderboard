a3b2f59ac03069a086b639ba8955c1cffa4d6102
Nabeel Akbar <studiosnabeel@gmail.com>
2022-11-24 18:58:23 +0500

    import addData and AddScoreList to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './style.css';
+import { addData, AddScoreList } from './Modules/api.js';
+
+const myForm = document.querySelector('#myForm');
+const refrshBtn = document.querySelector('.form-btn');
+
+AddScoreList();
+
+myForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const pName = myForm.pName.value.trim();
+  const pScore = myForm.pScore.value;
+  if (pName && pScore) {
+    addData(pName, pScore);
+    myForm.pName.value = '';
+    myForm.pScore.value = '';
+  }
+});
+
+refrshBtn.addEventListener('click', () => {
+  AddScoreList();
+});
