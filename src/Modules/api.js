5b4cba91589d98c529df9ed4fc7ee1c58cee88c6
Nabeel Akbar <studiosnabeel@gmail.com>
2022-11-24 18:49:28 +0500

    add async function

--- src/Modules/api.js
+++ src/Modules/api.js
@@ -1,4 +1,40 @@
+import addTableData from './data.js';
+import addTable from './data.js';
+
 const apiKey = 'XS8cvDPW4bUyTPeVkxvs';
 const baseUrl =
   'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
 const apiUrl = `${baseUrl}${apiKey}/scores`;
+
+const addData = async (player, score) => {
+  await fetch (apiUrl, {
+    method: 'POST',
+    body: JSON.stringify({
+     user: player,
+     score: score,
+    }),
+    headers: {
+      'content-type': 'application/json'; charset='UTF-8',
+    },
+  })
+  .then((response)=> response.json())
+  .then((json)=> {
+    const alrt = document.querySelector('h4');
+    alrt.innerHTML = json.result;
+    alrt.style.backgroundColor = '#fff';
+    alrt.style.color = 'red';
+    setTimeout(()=> {
+      alrt.style.display = 'none';
+}, 4000);
+  });
+};
+
+const AddScoreList = async ()=> {
+  await fetch (apiUrl)
+  .then((response)=> response.json())
+  .then((json)=> {
+    addTableData(json.result.sort((x, y)=> y.score - x.score));
+  });
+};
+
+export { addData,AddScoreList};
